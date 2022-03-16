d5f928066b96c176408fe524472a394027006379
gabrielribpar <96451742+gabrielribpar@users.noreply.github.com>
2022-03-16 12:25:13 -0300

    Making a new dr making a new dir

--- Ex3.py
+++
@@ -1,9 +0,0 @@
-def sum_to(n):
-    """Realiza a soma dos inteiro de 1 a n"""
-    soma=0 #Variavel auxiliar para ter a soma dos valores
-    for i in range(n+1): #Loop para realizar a soma dos valores
-        soma=soma+i
-    return soma
-
-
-print(sum_to(10))
