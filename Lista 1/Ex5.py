7d6b876eff86bd08546626e0d4a6d9aa76e9cef6
gabrielribpar <96451742+gabrielribpar@users.noreply.github.com>
2022-06-07 09:29:37 -0300

    Eliminando 1B

--- Lista 1/Ex5.py
+++
@@ -1,8 +0,0 @@
-def verifica_palindromo(palavra):
-    """Verifica se a palavra é um palindromo"""
-    palindromo=True #Variável que guarda se a palavra é um palindromo
-    for i in range(int(len(palavra)/2)): #Loop até o meio da palavra
-        if palavra[i]!=palavra[-1-i]:#verifica os extremos
-            palindromo=False
-            break 
-    return palindromo
